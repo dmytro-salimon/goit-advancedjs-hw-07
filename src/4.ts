fe7d7da7d2cebcdedd09e1a773e2043567bd31b7
dmytro-salimon <dmytro.salimon@gmail.com>
2025-01-19 23:17:47 +0200

    Complete tasks

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,69 @@
-const key = new Key();
+// Клас Key
+class Key {
+  private signature: number;
 
+  constructor() {
+    this.signature = Math.random();
+  }
+
+  public getSignature(): number {
+    return this.signature;
+  }
+}
+
+// Клас Person
+class Person {
+  private key: Key;
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  public getKey(): Key {
+    return this.key;
+  }
+}
+
+// Абстрактний клас House
+abstract class House {
+  protected door: boolean = false;
+  protected key: Key;
+  protected tenants: Person[] = [];
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  public comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log(`${person.getKey().getSignature()} has entered the house.`);
+    } else {
+      console.log('The door is closed. Cannot enter.');
+    }
+  }
+
+  public abstract openDoor(key: Key): void;
+}
+
+// Клас MyHouse
+class MyHouse extends House {
+  public openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+      console.log('Door is now open.');
+    } else {
+      console.log('Key does not match. Door remains closed.');
+    }
+  }
+}
+
+// Сценарій
+const key = new Key();
 const house = new MyHouse(key);
 const person = new Person(key);
 
 house.openDoor(person.getKey());
-
 house.comeIn(person);
-
 
 export {};
